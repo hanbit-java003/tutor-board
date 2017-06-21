e46326bdfae11b9c4f092cb6f6b177338fa2be6a
Pahnki Kim <pahnki.kim@pkworks.io>
2017-06-21 12:34:52 +0900

    add client validation

--- webapp/js/new.js
+++ webapp/js/new.js
@@ -6,4 +6,28 @@ $(function() {
 	$('#board-reset').on('click', function() {
 		$('#board-form')[0].reset();
 	});
+	
+	$('#board-send').on('click', function() {
+		// Form Validation (폼 검증)
+		if ($('#board-title').val().trim() === '') {
+			alert('제목을 입력하세요.');
+			$('#board-title').val('');
+			$('#board-title').focus();
+			return;
+		}
+		else if ($('#board-writer').val().trim() === '') {
+			alert('작성자를 입력하세요.');
+			$('#board-writer').val('');
+			$('#board-writer').focus();
+			return;
+		}
+		else if ($('#board-contents').val().trim() === '') {
+			alert('내용을 입력하세요.');
+			$('#board-contents').val('');
+			$('#board-contents').focus();
+			return;
+		}
+		
+		$('#board-form').submit();
+	});
 });
